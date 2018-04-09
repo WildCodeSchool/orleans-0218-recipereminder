645866741f67d6825ae37c6262f4db00eab387ca
Matt Crl <matth.carlevaris@gmail.com>
2018-04-09 16:49:04 +0200

    correction done after first pull request

--- app/routing.php
+++ app/routing.php
@@ -10,13 +10,6 @@
  */
 
 $routes = [
-    /*'Item' => [ // Controller
-        ['index', '/', 'GET'], // action, url, method
-        ['add', '/item/add', 'GET'], // action, url, method
-        ['edit', '/item/edit/{id:\d+}', 'GET'], // action, url, method
-        ['show', '/item/{id:\d+}', 'GET'], // action, url, method
-    ],*/
-
     'Homepage' => [
         ['index', '/', 'GET'],
     ]
