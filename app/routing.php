1f8da54ca1d75d950a029a5f2696b49c9543a384
Anthony POINTU <p.anhtony18@live.fr>
2018-04-10 17:53:48 +0200

    modif erreur boucle twig

--- app/routing.php
+++ app/routing.php
@@ -11,7 +11,7 @@
 
 
 $routes = [
-    /* 'Item' => [ // Controller
+     /*'Item' => [ // Controller
         ['index', '/', 'GET'], // action, url, method
         ['add', '/item/add', 'GET'], // action, url, method
         ['edit', '/item/edit/{id:\d+}', 'GET'], // action, url, method
