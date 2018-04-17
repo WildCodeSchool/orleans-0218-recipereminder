0b62e9a1d866f71bac6a312ca0fa74fef64deef7
Yoan BEAUCHAMP <yoan.beauchamp@gmail.com>
2018-04-17 13:14:31 +0200

    changement de la méthod de la route

--- app/routing.php
+++ app/routing.php
@@ -31,7 +31,7 @@ $routes = [
         ['showRecipe', '/recipe/{id:\d+}', 'GET'],
         ['addRecipe', '/admin/addRecipe', ['GET', 'POST']],
         ['adminlistRecipe', '/admin/recipeList', 'GET'], // action, url, method
-        ['searchRecipe', '/recipe/search', ['GET', 'POST']], // action, url, method
+        ['searchRecipe', '/recipe/search', 'POST'], // action, url, method
 
     ],
 
