5429fabad1e15498a7d5929e954d3917aebbaabb
Raphael Mouton <raphaelmouton@live.fr>
2018-04-18 13:57:00 +0200

    modification css thumb

--- public/assets/js/searchEvent.js
+++ public/assets/js/searchEvent.js
@@ -5,10 +5,9 @@ $('#formSeekEvent').submit(function (e) {
 });
 
 $('#seekEvent').keyup(function () {
-    console.log($(this).val())
 
     // on lance la fonction seulement si au moins 2 lettre dans le input, ou 0
-    if($(this).val().length > 2 || $(this).val().length === 0) {
+    if($(this).val().length > 1 || $(this).val().length === 0) {
         let event = $(this).val();
         $.post("/event/search", {event: event}).done(function (html) {
             $('#list').html(html);
