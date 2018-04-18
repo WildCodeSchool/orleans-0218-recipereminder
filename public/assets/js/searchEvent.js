8bf0b623d260546b7ddefeb23d4306aae61d767b
Raphael Mouton <raphaelmouton@live.fr>
2018-04-18 14:21:58 +0200

    CodeSnif Search Event

--- public/assets/js/searchEvent.js
+++ public/assets/js/searchEvent.js
@@ -7,7 +7,7 @@ $('#formSeekEvent').submit(function (e) {
 $('#seekEvent').keyup(function () {
 
     // on lance la fonction seulement si au moins 2 lettre dans le input, ou 0
-    if($(this).val().length > 1 || $(this).val().length === 0) {
+    if($(this).val().length > 2 || $(this).val().length === 0) {
         let event = $(this).val();
         $.post("/event/search", {event: event}).done(function (html) {
             $('#list').html(html);
