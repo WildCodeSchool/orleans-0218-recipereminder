0298c6ad35ae6fdb0cb24e53ef2546799d2d0948
Yoan BEAUCHAMP <yoan.beauchamp@gmail.com>
2018-04-18 09:45:29 +0200

    On empeche la req ajax de se lancer si le nombre de letttre est < à 2

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,8 +5,12 @@ $('#formSeekRecipe').submit(function (e) {
 });
 
 $('#seekRecipe').keyup(function () {
-    let recipe = $(this).val();
-    $.post("/recipe/search", { recipe: recipe }).done(function (html) {
-        $('#list').html(html);
-    });
+
+    // on lance la fonction seulement si au moins 2 lettre dans le input, ou 0
+    if($(this).val().length > 2 || $(this).val().length === 0) {
+        let recipe = $(this).val();
+        $.post("/recipe/search", {recipe: recipe}).done(function (html) {
+            $('#list').html(html);
+        });
+    }
 });
