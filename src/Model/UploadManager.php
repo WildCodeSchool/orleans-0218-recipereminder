24ae165a816031ba80441da0887a99beede623f4
Matt Crl <matth.carlevaris@gmail.com>
2018-04-19 09:11:35 +0200

    rectif upload img vide

--- src/Model/UploadManager.php
+++ src/Model/UploadManager.php
@@ -24,15 +24,18 @@ class UploadManager
             if ($file["img"]["size"] > self::MAXSIZE) {
                 throw new \Exception('Votre image devrait faire moins de '. self::MAXSIZE . 'ko');
             }
+
             $imageFileType = strtolower(pathinfo($uploadFile, PATHINFO_EXTENSION));
+
             if ($imageFileType == null) {
-                $filename = '';
-                return $filename;
+                return '';
             }
+
             $extensionAuth = ["jpg", "png", "gif"];
             if (!in_array($imageFileType, $extensionAuth)) {
                 throw new \Exception('Oups pas le bon format ' . implode(", ", $extensionAuth));
             }
+
             $extension = pathinfo($file['img']['name'], PATHINFO_EXTENSION);
             $filename = "Image" . uniqid() . '.' . $extension;
             move_uploaded_file($file['img']['tmp_name'], 'assets/upload/' . $filename);
