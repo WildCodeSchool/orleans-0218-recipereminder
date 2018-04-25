7aacd8bfcfc49ae2640974dcb786d678f256a2b6
Xavier Meckler <xmeckler@gmail.com>
2018-04-25 18:28:08 +0200

    Ajout d'un message d'ajout réussi et suppression du bouton d'ajout une fois la recette associée

--- public/assets/js/linkRecipesToOneEvent.js
+++ public/assets/js/linkRecipesToOneEvent.js
@@ -2,7 +2,9 @@
 $('.linkBtn').click(function(){
     let recipeId = $(this).attr('data-recipeId');
     let eventId = $('#list').attr('data-eventId');
-
+    let success = $('<p class="success"><span class="glyphicon glyphicon-thumbs-up"></span> <strong>Cette recette est maintenant liée à l\'événement !</strong></p>')
+    success.insertAfter($(this));
+    $(this).hide();
     $.post("/admin/event/linkRecipeToEvent", {recipeId:recipeId,eventId:eventId}).done(function (html) {// ('url de l'action , tous le contenu du form)
         console.log(html);
     });
